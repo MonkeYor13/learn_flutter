b7f7d36eebf9229a18f4326773144b17a766e2d3
“MonkeYor13” <monkeyor13@gmail.com>
2023-11-02 14:34:53 -0300

    lista de ocntacto dinamica y se agrega random de colores

--- lib/screens/contactos_screen.dart
+++ lib/screens/contactos_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/model/contact_user.dart';
 
@@ -8,6 +10,9 @@ class ContactsScreen extends StatelessWidget {
     ContactUser(nameContact: 'Pedro', phone: '54955555555', type: 'Movil'),
     ContactUser(nameContact: 'Maria', phone: '54955555555', type: 'Movil'),
     ContactUser(nameContact: 'Rufilin', phone: '54955555555', type: 'Home'),
+    ContactUser(nameContact: 'Cesar', phone: '54955555555', type: 'Movil'),
+    ContactUser(nameContact: 'Juan', phone: '54955555555', type: 'Home'),
+    ContactUser(nameContact: 'Pepe', phone: '54955555555', type: 'Movil'),
   ];
 
   @override
@@ -59,15 +64,95 @@ class ContactsScreen extends StatelessWidget {
               ],
             ),
           ),
+          SizedBox(
+            height: 180.0,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                //shrinkWrap: true,
+                itemCount: favoriteContacts.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final favoriteContact = favoriteContacts[index];
+                  return _FavoriteContact(favoriteContact: favoriteContact);
+                }),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Text(
+              'FRECUENTES',
+              style: TextStyle(fontSize: 15.0, color: Colors.black54),
+            ),
+          ),
           ListView.builder(
+              physics: BouncingScrollPhysics(),
               shrinkWrap: true,
               itemCount: favoriteContacts.length,
               itemBuilder: (BuildContext context, int index) {
                 final favoriteContact = favoriteContacts[index];
-                return Text(favoriteContact.nameContact);
+                return ListTile(
+                  title: Text(favoriteContact.nameContact),
+                  subtitle: Text('${favoriteContact.type} ${favoriteContact.phone}'),
+                );
               }),
         ],
       ),
+    );
+  }
+}
+
+class _FavoriteContact extends StatelessWidget {
+  const _FavoriteContact({
+    required this.favoriteContact,
+  });
+
+  final ContactUser favoriteContact;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          //alignment: AlignmentDirectional.bottomEnd,
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+              height: 70.0,
+              width: 70.0,
+              child: CircleAvatar(
+                child: Text(
+                  favoriteContact.nameContact.substring(0, 1),
+                  style: TextStyle(color: Colors.amber, fontSize: 20.0),
+                ),
+                backgroundColor: Color.fromRGBO(Random().nextInt(255),
+                    Random().nextInt(255), Random().nextInt(255), 1),
+              ),
+            ),
+            Positioned(
+              right: 0,
+              bottom: 3,
+              child: MaterialButton(
+                onPressed: () {},
+                child: Icon(
+                  Icons.phone,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100.0),
+                ),
+                color: Colors.white,
+                height: 50.0,
+                minWidth: 10.0,
+              ),
+            ),
+          ],
+        ),
+        Text(
+          favoriteContact.nameContact,
+          style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          favoriteContact.type,
+          style: TextStyle(fontSize: 10.0),
+        ),
+      ],
     );
   }
 }
